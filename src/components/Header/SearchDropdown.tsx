7c615d4dfdcf2df8a82e6b5a217f46d6ab0418b9
yujin42 <4yujin2@gmail.com>
2025-12-23 17:07:22 +0900

    캐릭터명추가

--- src/components/Header/SearchDropdown.tsx
+++ src/components/Header/SearchDropdown.tsx
@@ -51,16 +51,17 @@ const SearchDropdown: React.FC<SearchDropdownProps> = ({
       return `/detail/${result.id}`;
     } else if (result.media_type === 'movie') {
       return `/movie/detail/${result.id}`;
-    } else {
-      // person 타입은 개인 프로필 페이지로 이동
+    } else if (result.media_type === 'person') {
       return `/person/${result.id}`;
     }
+    return undefined;
   };
 
   const getMediaType = (type: string) => {
     if (type === 'tv') return 'TV';
     if (type === 'movie') return '영화';
     if (type === 'person') return '출연진';
+    if (type === 'character') return '캐릭터';
     return '';
   };
 
@@ -68,46 +69,87 @@ const SearchDropdown: React.FC<SearchDropdownProps> = ({
     <div className="search-dropdown">
       <div className="search-results">
         {results.map((result, index) => (
-          <Link
-            key={`${result.media_type}-${result.id}`}
-            ref={selectedIndex === index ? selectedItemRef : null}
-            to={getMediaLink(result)}
-            className={`search-item ${result.media_type === 'person' ? 'is-person' : ''} ${
-              selectedIndex === index ? 'selected' : ''
-            }`}
-            onClick={() => {
-              onClose();
-              onNavigate?.(result);
-            }}
-            onMouseEnter={() => onIndexChange?.(index)}
-            onMouseLeave={() => onIndexChange?.(-1)}>
-            <div className="search-item-poster">
-              {result.poster_path ? (
-                <img
-                  src={`https://image.tmdb.org/t/p/w92${result.poster_path}`}
-                  alt={result.title}
-                />
-              ) : (
-                <div className="no-poster">No Image</div>
-              )}
-            </div>
-            <div className="search-item-info">
-              <div className="search-item-title">
-                {result.title}
-                <span className="media-type">{getMediaType(result.media_type)}</span>
+          result.media_type === 'character' ? (
+            <div
+              key={`${result.media_type}-${result.id}`}
+              className={`search-item is-character ${selectedIndex === index ? 'selected' : ''}`}
+              onMouseEnter={() => onIndexChange?.(index)}
+              onMouseLeave={() => onIndexChange?.(-1)}
+            >
+              <div className="search-item-poster">
+                {result.poster_path ? (
+                  <img
+                    src={result.poster_path.startsWith('/images/') ? result.poster_path : result.poster_path.startsWith('http') ? result.poster_path : `https://image.tmdb.org/t/p/w92${result.poster_path}`}
+                    alt={result.title}
+                  />
+                ) : (
+                  <div className="no-poster">No Image</div>
+                )}
               </div>
-              {result.media_type === 'person' && result.known_for_department && (
-                <div className="search-item-overview">{result.known_for_department}</div>
-              )}
-              {result.media_type !== 'person' && result.overview && (
-                <div className="search-item-overview">
-                  {result.overview.length > 80
-                    ? `${result.overview.substring(0, 80)}...`
-                    : result.overview}
+              <div className="search-item-info">
+                <div className="search-item-title">
+                  {result.title}
+                  <span className="media-type">{getMediaType(result.media_type)}</span>
                 </div>
-              )}
+                {result.known_for_department && (
+                  <div className="search-item-overview">{result.known_for_department}</div>
+                )}
+                {result.overview && (
+                  <div className="search-item-overview">
+                    {result.overview.length > 80
+                      ? `${result.overview.substring(0, 80)}...`
+                      : result.overview}
+                  </div>
+                )}
+              </div>
             </div>
-          </Link>
+          ) : (
+            <Link
+              key={`${result.media_type}-${result.id}`}
+              ref={selectedIndex === index ? selectedItemRef : null}
+              to={getMediaLink(result) || '#'}
+              className={`search-item ${result.media_type === 'person' ? 'is-person' : ''} ${
+                selectedIndex === index ? 'selected' : ''
+              }`}
+              onClick={() => {
+                onClose();
+                onNavigate?.(result);
+              }}
+              onMouseEnter={() => onIndexChange?.(index)}
+              onMouseLeave={() => onIndexChange?.(-1)}
+            >
+              <div className="search-item-poster">
+                {result.poster_path ? (
+                  <img
+                    src={
+                      result.poster_path.startsWith('/images/') || result.poster_path.startsWith('http')
+                        ? result.poster_path
+                        : `https://image.tmdb.org/t/p/w92${result.poster_path}`
+                    }
+                    alt={result.title}
+                  />
+                ) : (
+                  <div className="no-poster">No Image</div>
+                )}
+              </div>
+              <div className="search-item-info">
+                <div className="search-item-title">
+                  {result.title}
+                  <span className="media-type">{getMediaType(result.media_type)}</span>
+                </div>
+                {result.media_type === 'person' && result.known_for_department && (
+                  <div className="search-item-overview">{result.known_for_department}</div>
+                )}
+                {result.media_type !== 'person' && result.overview && (
+                  <div className="search-item-overview">
+                    {result.overview.length > 80
+                      ? `${result.overview.substring(0, 80)}...`
+                      : result.overview}
+                  </div>
+                )}
+              </div>
+            </Link>
+          )
         ))}
       </div>
       {results.length > 0 && searchQuery && (
