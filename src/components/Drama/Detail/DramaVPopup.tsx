58072a1ae18e30ebcff15c493f2c0de85eb8970a
wow-woody <jjeongwuss1@gmail.com>
2026-01-13 16:46:48 +0900

    fix: 컨텐츠 너비 제약 제거 - 1920px 전체 너비 사용
    - 모든 페이지의 padding-left: 10.8rem (108px) 제거
    - 모든 섹션의 padding-right: 2.4rem (24px) 제거
    - 컨텐츠가 이제 화면 전체 너비(1920px)를 사용
    - 영향받은 파일들:
      - Home.scss, Movie.scss, Enter.scss, Drama.scss
      - Anim.scss, DramaGenre.scss, AnimGenre.scss
      - MovieGenre.scss, DramaDetail.scss, MovieDetail.scss
      - Sport.scss (및 swiper 버튼 위치 조정)

--- src/components/Drama/Detail/DramaVPopup.tsx
+++ src/components/Drama/Detail/DramaVPopup.tsx
@@ -1,28 +1,28 @@
 import '../../Common/YoutubeModal.scss';
 
 interface VideoPopupProps {
-  youtubeKey: string;
-  onClose: () => void;
+    youtubeKey: string;
+    onClose: () => void;
 }
 
 const DramaVPopup = ({ youtubeKey, onClose }: VideoPopupProps) => {
-  return (
-    <div className="youtube-modal__backdrop" onClick={onClose}>
-      <div className="youtube-modal__content" onClick={(e) => e.stopPropagation()}>
-        <button className="youtube-modal__close" onClick={onClose}>
-          ✕
-        </button>
-        <div className="youtube-modal__video-box">
-          <iframe
-            src={`https://www.youtube.com/embed/${youtubeKey}?autoplay=1`}
-            title="YouTube video player"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-          />
+    return (
+        <div className="youtube-modal__backdrop" onClick={onClose}>
+            <div className="youtube-modal__content" onClick={(e) => e.stopPropagation()}>
+                <button className="youtube-modal__close" onClick={onClose}>
+                    ✕
+                </button>
+                <div className="youtube-modal__video-box">
+                    <iframe
+                        src={`https://www.youtube.com/embed/${youtubeKey}?autoplay=1`}
+                        title="YouTube video player"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                        allowFullScreen
+                    />
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default DramaVPopup;
